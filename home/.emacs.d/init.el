2d6220258931c3513870c366cd19af7d59894802
Steven Degutis <sbdegutis@gmail.com>
2019-06-27 15:32:38 -0500

    Not convinced I need or use these.

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -243,16 +243,16 @@
 
   ;; ivy fuzzy matching
   (setq ivy-re-builders-alist
-        '((swiper . regexp-quote)
+        '(;; (swiper . regexp-quote)
           (t      . ivy--regex-fuzzy)))
 
   (global-set-key (kbd "M-x")     'counsel-M-x)
   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
   (global-set-key (kbd "<f1> f")  'counsel-describe-function)
   (global-set-key (kbd "<f1> v")  'counsel-describe-variable)
-  (global-set-key (kbd "<f1> l")  'counsel-find-library)
-  (global-set-key (kbd "<f2> i")  'counsel-info-lookup-symbol)
-  (global-set-key (kbd "<f2> u")  'counsel-unicode-char)
+;;   (global-set-key (kbd "<f1> l")  'counsel-find-library)
+;;   (global-set-key (kbd "<f2> i")  'counsel-info-lookup-symbol)
+;;   (global-set-key (kbd "<f2> u")  'counsel-unicode-char)
   (global-set-key (kbd "C-c g")   'counsel-git)
   (global-set-key (kbd "C-c j")   'counsel-git-grep)
   (global-set-key (kbd "C-x b")   'ivy-switch-buffer)
