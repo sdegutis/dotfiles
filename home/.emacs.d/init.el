a7ac2402dea4a3cea990c06bc3d1f16657ebc442
Steven Degutis <sbdegutis@gmail.com>
2019-06-27 16:59:59 -0500

    Easier to remember install-package shortcut.

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -272,8 +272,7 @@
   (global-set-key (kbd "C-c g")   'counsel-git)
   (global-set-key (kbd "C-c j")   'counsel-git-grep)
   (global-set-key (kbd "C-x b")   'ivy-switch-buffer)
-  (global-set-key (kbd "C-c C-r") 'ivy-resume)
-  (global-set-key (kbd "C-x C-p") 'counsel-package)
+  (global-set-key (kbd "C-x p")   'counsel-package)
 
   (define-key ivy-switch-buffer-map (kbd "C-k") 'ivy-switch-buffer-kill)
 
