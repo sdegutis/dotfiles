c6fb400940df7344790b896d77822c1998cc41ac
Steven Degutis <sbdegutis@gmail.com>
2019-06-27 21:08:30 -0500

    Cmd-F = swiper (ivy-based search)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -293,6 +293,7 @@
 
   (global-set-key (kbd "M-x")     'counsel-M-x)
   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "s-f")     'swiper)
   (global-set-key (kbd "<f1> f")  'counsel-describe-function)
   (global-set-key (kbd "<f1> v")  'counsel-describe-variable)
 ;;   (global-set-key (kbd "<f1> l")  'counsel-find-library)
