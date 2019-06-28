3ed7aeef8d32b4c7af2b58279b951bf4be66c534
Steven Degutis <sbdegutis@gmail.com>
2019-06-28 09:10:18 -0500

    Removing eshell's xterm-color support because it seems actually worse.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -121,9 +121,6 @@
    ;; git
    magit
 
-   ;; eshell
-   xterm-color
-
    ;; fuzzy-completion
    counsel
    flx
@@ -412,19 +409,8 @@
       (eshell/pwd))
      (if (= (user-uid) 0) " # " " $ ")))
 
-  ;; ostensibly better color support
-  (add-hook 'eshell-before-prompt-hook
-            (lambda ()
-              (setq xterm-color-preserve-properties t)))
-  (add-to-list 'eshell-preoutput-filter-functions 'xterm-color-filter)
-  (setq eshell-output-filter-functions
-        (remove 'eshell-handle-ansi-color
-                eshell-output-filter-functions))
-
   ;; (add-hook 'eshell-mode-hook 'toggle-truncate-lines)
-  (add-hook 'eshell-mode-hook #'sd/setup-eshell)
-  ;; (add-hook 'eshell-before-prompt-hook (lambda () (setq xterm-color-preserve-properties t)))
-  )
+  (add-hook 'eshell-mode-hook #'sd/setup-eshell))
 
 
 
